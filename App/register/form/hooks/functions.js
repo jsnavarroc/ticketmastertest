6594644c46883e9dc7e9ba103ba3134998cc1683
jnavarro <21sebas12@gmail.com>
2021-06-06 18:14:00 +0100

    remove parameter

--- App/register/form/hooks/functions.js
+++ App/register/form/hooks/functions.js
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-export const configureYup = workflowEdit => {
+export const configureYup = () => {
   const phoneRegExp =
     /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
